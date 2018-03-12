f646bd73e76dee940418633df5826957017e6e21
Juan Cuadros <juan.josecuadros1@gmail.com>
2018-03-11 19:38:42 -0500

    mail changed to gerencia

--- mail/reserva.php
+++ mail/reserva.php
@@ -22,12 +22,12 @@ $personas = strip_tags(htmlspecialchars($_POST['personas']));
 $hostal = strip_tags(htmlspecialchars($_POST['hostal']));
 
 // Create the email and send the message
-$to = 'jennyepes@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'gerencia@f2b.com.co'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Haz recibido un correo de RESERVA.\n\n"."Los detalles:
 \n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nDateIn: $dateIn\n\nDateOut: $dateOut\n\nPersonas:$personas\n\nHostal:$hostal ";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
 ?>
